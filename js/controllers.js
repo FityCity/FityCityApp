60ccbc5e37a26c993d2a317d8378aee29a0a5fdb
zym242 <zymfrank@hotmail.com>
2014-09-08 11:37:07 +0930

    login page demo

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,28 @@
 angular.module('starter.controllers', [])
+        .controller('LoginCtrl', function($scope, $location) {
+            $scope.login = function() {
+                $location.path("/tab/dash")
+            }
+        })
+        .controller('DashCtrl', function($scope) {
+        })
 
-.controller('DashCtrl', function($scope) {
-})
+        .controller('FriendsCtrl', function($scope, Friends) {
+            $scope.friends = Friends.all();
+        })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+        .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+            $scope.friend = Friends.get($stateParams.friendId);
+        })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+        .controller('VendorsCtrl', function($scope, Vendors) {
+            $scope.vendors = Vendors.all();
+        })
 
-.controller('VendorsCtrl', function($scope, Vendors) {
-  $scope.vendors = Vendors.all();
-})
+        .controller('VendorDetailCtrl', function($scope, $stateParams, Vendors, Activities) {
+            $scope.vendor = Vendors.get($stateParams.vendorId);
+            $scope.cards = Activities.all();
+        })
 
-.controller('VendorDetailCtrl', function($scope, $stateParams, Vendors, Activities) {
-  $scope.vendor = Vendors.get($stateParams.vendorId);
-  
-  $scope.cards = Activities.all();
-})
-
-.controller('AccountCtrl', function($scope) {
-});
+        .controller('AccountCtrl', function($scope) {
+        });
