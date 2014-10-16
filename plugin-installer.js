9a5ef3b093d0a8b91899ab9b8faa42b56e834a35
zym242 <zymfrank@hotmail.com>
2014-10-16 14:38:44 +1030

    facebook login installer

--- plugin-installer.js
+++ plugin-installer.js
@@ -5,3 +5,15 @@ var exec = require('child_process').exec;
 exec('cordova plugin add https://github.com/phonegap-build/PushPlugin.git', function (error, stdout, stderr) {
       console.log(stdout, error, stderr)
 });
+
+exec('cordova -d plugin add https://github.com/phonegap/phonegap-facebook-plugin.git --variable APP_ID="1552086008346502" --variable APP_NAME="fitecity"', function (error, stdout, stderr) {
+      console.log(stdout, error, stderr)
+});
+
+exec('android update project --subprojects --path "platforms/android" --target android-19 --library "CordovaLib"', function (error, stdout, stderr) {
+      console.log(stdout, error, stderr)
+});
+
+exec('android update project --subprojects --path "platforms/android" --target android-19 --library "FacebookLib"', function (error, stdout, stderr) {
+      console.log(stdout, error, stderr)
+});
